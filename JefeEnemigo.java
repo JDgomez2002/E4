69fe13839e8e9e21c3cb17d546e1984e297ea69e
JDgomez2002 <gom21429@uvg.edu.gt>
2021-09-28 18:06:07 -0600

    Creación de Clases

---
+++ JefeEnemigo.java
@@ -0,0 +1,12 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//José Daniel Gómez Cabrera
+//Carné 21429
+//Sección 11
+//Actividad: Ejercicio 4
+
+public class JefeEnemigo extends Enemigo{
+    
+}
